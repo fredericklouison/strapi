bc6c772cc0377e1b4589aeddb0fea821cb142dfb
fredolouis <63505296+fredolouis@users.noreply.github.com>
2020-10-12 11:52:07 +0200

    second commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="circle_One">
+    
+      </div>
+      <div id="circle_Two">
+    
+      </div>
+      <div id="circle_Three">
+
+      </div>
+      <div id='text'>
+          <h1>Fréderick<span> LOUISON</span></h1>
+          <h2><span>Développeur </span>Web</h2>
+          <h3>Bienvenue sur mon Portfolio</h3>
+          <div id='enterAnim'>
+          <i className="fas fa-chevron-right"></i>
+          <i className="fas fa-chevron-right"></i>
+          <i className="fas fa-chevron-right"></i>
+        </div>
+      </div>
     </div>
   );
 }
